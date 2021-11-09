70e5036cf637e60ff229ad479b40d2ac4a03f67b
Wenjiang Tang <wenjiang.tang@kingsgroupgames.com>
2021-11-09 17:24:21 +0800

    dockerfile updage

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,15 @@
 FROM ubuntu:latest
 MAINTAINER twjitm "twjitm@gmail.com"
-RUN apt-get update
-CMD ['/usr/sbin/init']
+COPY packages/jdk-8-linux-x64.tar.gz /root/
+RUN apt-get update &&\
+    mkdir -p /usr/lib/java &&\
+    tar -zxvf /root/jdk-8-linux-x64.tar.gz -C /usr/lib/java
+
+
+
+ENV JAVA_HOME=/usr/lib/java/jdk1.8.0
+ENV JRE_HOME ${JAVA_HOME}/jre
+ENV CLASSPATH .:${JAVA_HOME}/lib:${JRE_HOME}/lib
+ENV PATH ${JAVA_HOME}/bin:$PATH
+
+CMD ["/bin/bash"]
--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
-#!/usr/bom/env bash
+#!/sh/bash
 echo 'start run  docker'
-ip='127.0.0.1'
+PORT=$1
+WEB_PORT=$((1100 + ${PORT}))
+FTP_PORT=$((1200 + ${PORT}))
+MONGO_PORT=$((1300 + ${PORT}))
 
-docker run -m 2g -it -d --privileged -p 2222:22 -v //Users/toufumie/twjitm/twjitm-game/:/data/code/twjitm-game/ twjitm:test /usr/sbin/init >maintainer.pid
+docker run -m 2g -it -d --privileged -p "${MONGO_PORT}":27017 -p "${WEB_PORT}":80 -p "${FTP_PORT}":22 twjitm:test /bin/sh >maintainer.pid
 pid=$(cat maintainer.pid)
 echo $pid
 
